2d925cb03f8d03b2717f1f1422034b900fa11a52
Christopher Adigun <futuredon@gmail.com>
2024-06-14 23:20:48 +0000

    Update workload-cluster package

--- workload-eks-tf-module-github/module/main.tf
+++ workload-eks-tf-module-github/module/main.tf
@@ -45,6 +45,7 @@ module "eks" {
   cluster_endpoint_public_access = true
   cluster_endpoint_private_access = false
   enable_cluster_creator_admin_permissions = true
+  create_kms_key                 = false
 
   cluster_addons = {
     coredns = {
