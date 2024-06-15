df33f6cd033cb2ad1fb99875740260d8fbae42a0
Christopher Adigun <futuredon@gmail.com>
2024-06-15 01:41:54 +0000

    Update workload-cluster package

--- workload-eks-tf-module-github/module/main.tf
+++ workload-eks-tf-module-github/module/main.tf
@@ -92,7 +92,7 @@ module "eks" {
       enable_bootstrap_user_data = true
       post_bootstrap_user_data = <<-EOT
         #!/bin/bash
-        set -o xtrace
+        set -x
         echo "net.ipv4.conf.default.rp_filter = 0" | tee -a /etc/sysctl.conf
         echo "net.ipv4.conf.all.rp_filter = 0" | tee -a /etc/sysctl.conf
         sudo sysctl -p
@@ -100,7 +100,8 @@ module "eks" {
         ls /sys/class/net/ > /tmp/ethList;cat /tmp/ethList |while read line ; do sudo ifconfig $line up; done
         grep eth /tmp/ethList |while read line ; do echo "ifconfig $line up" >> /etc/rc.d/rc.local; done
         chmod +x /etc/rc.d/rc.local
-        systemctl enable rc-local --now    
+        systemctl enable rc-local --now
+        reboot
       EOT
       tags = {
         Terraform   = "true"
