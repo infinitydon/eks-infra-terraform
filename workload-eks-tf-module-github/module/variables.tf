80518c5b03e2548b2b7f08147f3101ac41d47eb7
Christopher Adigun <futuredon@gmail.com>
2024-07-27 00:54:08 +0000

    revert increase node count

--- workload-eks-tf-module-github/module/variables.tf
+++ workload-eks-tf-module-github/module/variables.tf
@@ -57,15 +57,15 @@ variable "git_credentials_secret" {
 }
 
 variable "node_instance_desired_capacity" {
-  default = "2"
+  default = "1"
 }
 
 variable "node_instance_max_capacity" {
-  default = "3"
+  default = "2"
 }
 
 variable "node_instance_min_capacity" {
-  default = "2"
+  default = "1"
 }
 
 variable "multus_subnets" {
