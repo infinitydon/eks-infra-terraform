5c986387c7c02dc68d033d4cd4173961e329b83a
Christopher Adigun <futuredon@gmail.com>
2024-08-23 00:12:22 +0000

    Fix AZ list

--- mgmt-eks-tf-module-github/module/main.tf
+++ mgmt-eks-tf-module-github/module/main.tf
@@ -7,7 +7,7 @@ locals {
   region          = var.region
 
   vpc_cidr = "10.10.0.0/16"
-  azs      = slice(data.aws_availability_zones.available.names, 2, 5)
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
   tags = {
     Example    = local.name
