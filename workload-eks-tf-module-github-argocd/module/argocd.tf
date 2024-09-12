b48272ab3376d8a56ab9d8d5c78ab64d8baf8121
Christopher Adigun <futuredon@gmail.com>
2024-09-12 23:49:59 +0000

    Add fluentbit and container-insights in workload tf

--- workload-eks-tf-module-github-argocd/module/argocd.tf
+++ workload-eks-tf-module-github-argocd/module/argocd.tf
@@ -14,7 +14,13 @@ module "eks_blueprints_addons" {
   cluster_name      = module.eks.cluster_name
   cluster_endpoint  = module.eks.cluster_endpoint
   cluster_version   = module.eks.cluster_version
-  oidc_provider_arn = module.eks.oidc_provider_arn  
+  oidc_provider_arn = module.eks.oidc_provider_arn
+
+  enable_aws_cloudwatch_metrics = true
+  enable_aws_for_fluentbit = true
+  aws_for_fluentbit = {
+    enable_containerinsights = true
+  }  
 
   enable_aws_load_balancer_controller  = var.aws_load_balancer_controller_enable
   enable_argocd = true
